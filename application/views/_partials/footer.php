57a17302c3e8e05a5aa71c81abcd56fc52c53018
Sukma <ayuusukma714@gmail.com>
2026-05-10 20:31:55 +0800

    fix footer merge conflict

--- application/views/_partials/footer.php
+++ application/views/_partials/footer.php
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
-<footer class="bg-primary text-white text-center mt-5 p-3">
-    © 2026 Bengkel Maju Jaya Motor
-</footer>
-
-=======
 <footer class="bg-primary text-white text-center py-3 mt-5">
     <div class="container">
-        <small>© <?= date('Y') ?> Copyright</small>
+        <small>© <?= date('Y') ?> Bengkel Maju Jaya Motor</small>
     </div>
 </footer>
 
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
->>>>>>> ad84e4d10bba63d93c0e5bdc26e96fd7a0656dfa
 </body>
 </html>
