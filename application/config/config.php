20d2e42b7b5f572da84e3c0c490ed3b1cabe976c
Sukma <ayuusukma714@gmail.com>
2026-05-16 18:26:18 +0800

    Perbaiki konfigurasi database

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/MANAJEMEN_BENGKEL_MOTOR/';
-$config['base_url'] = 'http://localhost/db_bengkel_motor/';
+
 $config['index_page'] = '';
 
 /*
